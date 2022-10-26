78d09c0e2979c6693911f9dae497afe3ee1ba2ed
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-26 11:25:40 -0400

    resaltando pagina actual

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 const Layout = () => {
+  const location = useLocation();
   return (
     <div className="md:flex md:min-h-screen">
       <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -9,13 +10,19 @@ const Layout = () => {
         <nav className="mt-10">
           <Link
             to="/"
-            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            className={`${
+              location.pathname === "/" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300 text-white`}
           >
             Clientes
           </Link>
           <Link
             to="/clientes/nuevo"
-            className="text-2xl block mt-2 hover:text-blue-300 text-white"
+            className={`${
+              location.pathname === "/clientes/nuevo"
+                ? "text-blue-300"
+                : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300 text-white`}
           >
             Nuevo Cliente
           </Link>
