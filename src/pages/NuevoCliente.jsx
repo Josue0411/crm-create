2a1e7019c4f3aa9a983dd808b72181522d0c0b1e
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-26 23:56:45 -0400

    conectando json server al proyecto

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -4,13 +4,21 @@ import Error from "../components/Error";
 export async function action({ request }) {
   const formData = await request.formData();
   const datos = Object.fromEntries(formData);
+  const email = formData.get("email");
+
   //Validacion
   const errores = [];
   if (Object.values(datos).includes("")) {
     errores.push("Todos los campos son obligatorios");
   }
-  //Retornar si hay errores
+  let regex = new RegExp(
+    "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+  );
+  if (!regex.test(email)) {
+    errores.push("El email no es valido");
+  }
   if (Object.keys(errores).length) {
+    //Retornar si hay errores
     return errores;
   }
 }
@@ -35,7 +43,7 @@ function NuevoCliente() {
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
         {errores?.length &&
           errores.map((error, i) => <Error key={i}>{error} </Error>)}
-        <Form method="post">
+        <Form method="post" noValidate>
           <Formulario />
           <input
             type="submit"
